12d80b24f734bb2f31728f5b52f5320537f103a3
Brandon Hoynick <hoynick@ualberta.ca>
2024-09-10 14:20:53 -0600

    submitting part 1.7

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 
+// copilot suggested these, which i think are very good as they are decoupled from App's onClick functions
+const calcAll = (good, neutral, bad) => {
+  const all = good + neutral + bad
+  return all
+}
+
+const calcAverage = (good, neutral, bad) => {
+  const average = (good - bad) / calcAll(good, neutral, bad)
+  return average
+}
+
+const calcPositive = (good, neutral, bad) => {
+  const positive = (good / calcAll(good, neutral, bad)) * 100
+  return positive
+}
+
 function App() {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +24,7 @@ function App() {
 
   return (
     <div>
-      <h1>give feeback</h1>
+      <h1>give feedback</h1>
       <button onClick={() => setGood((good) => good + 1)}> good </button>
       <button onClick={() => setNeutral((neutral) => neutral + 1)}> neutral </button>
       <button onClick={() => setBad((bad) => bad + 1)}> bad </button>
@@ -17,6 +33,9 @@ function App() {
       <p> good {good} </p>
       <p> neutral {neutral} </p>
       <p> bad {bad} </p>
+      <p> all {calcAll(good, neutral, bad)} </p>
+      <p> average {calcAverage(good, neutral, bad)} </p>
+      <p> positive {calcPositive(good, neutral, bad)} % </p>
     </div>
   )
 }
