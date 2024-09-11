a123e3ced2638a7e442eebac3fa25a7af14a2b40
Brandon Hoynick <hoynick@ualberta.ca>
2024-09-10 19:32:24 -0600

    submitting part2.9

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -1,30 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "123 4567" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    { name: "arno bog", number: "45654662", id: 5 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newNameFilter, setNewNameFilter] = useState("");
 
   // capture the input value as you type
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
-    const handleNoteChangeNumber = (event) => {
-      console.log(event.target.value);
-      setNewNumber(event.target.value);
-    };
+  const handleNoteChangeNumber = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+  const handleNoteChangeNameFilter = (event) => {
+    console.log(event.target.value);
+    setNewNameFilter(event.target.value);
+  };
 
   const addNote = (event) => {
     event.preventDefault(); // prevents refreshing the page
     // reference: https://www.geeksforgeeks.org/how-to-check-if-an-array-includes-an-object-in-javascript/
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some((person) => person.name === newName)) {
       alert(`name ${newName} is already added to phonebook`);
       return;
-    } else if (persons.some(person => person.number === newNumber)) {
-        alert(`number ${newNumber} is already added to phonebook`);
-        return;
-      } else {
+    } else if (persons.some((person) => person.number === newNumber)) {
+      alert(`number ${newNumber} is already added to phonebook`);
+      return;
+    } else {
       const noteObject = {
         name: newName,
         number: newNumber,
@@ -38,20 +49,32 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={newNameFilter} onChange={handleNoteChangeNameFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNote}>
         <div>
-        name: <input value={newName} onChange={handleNoteChange} /> <br></br>
-        number: <input value={newNumber} onChange={handleNoteChangeNumber} />
+          name: <input value={newName} onChange={handleNoteChange} /> <br></br>
+          number: <input value={newNumber} onChange={handleNoteChangeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {/*  method chaining of .filter onto .map*/}
       <ul>
-        {persons.map((each) => (
-          <li key={each.name}>{each.name} at {each.number}</li>
-        ))}
+        {persons
+          .filter((each) =>
+            each.name.toLowerCase().includes(newNameFilter.toLowerCase())
+          )
+          .map((filteredPerson) => (
+            <li key={filteredPerson.name}>
+              {filteredPerson.name} at {filteredPerson.number}
+            </li>
+          ))}
       </ul>
     </div>
   );
