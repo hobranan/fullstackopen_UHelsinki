2034f9a79db12f38f751928c1676dd3a0e84e74a
Brandon Hoynick <hoynick@ualberta.ca>
2024-09-10 19:07:16 -0600

    submitting part2.8

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -1,37 +1,47 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "123 4567" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   // capture the input value as you type
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
+    const handleNoteChangeNumber = (event) => {
+      console.log(event.target.value);
+      setNewNumber(event.target.value);
+    };
 
   const addNote = (event) => {
     event.preventDefault(); // prevents refreshing the page
     // reference: https://www.geeksforgeeks.org/how-to-check-if-an-array-includes-an-object-in-javascript/
     if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+      alert(`name ${newName} is already added to phonebook`);
       return;
-    } else {
+    } else if (persons.some(person => person.number === newNumber)) {
+        alert(`number ${newNumber} is already added to phonebook`);
+        return;
+      } else {
       const noteObject = {
         name: newName,
+        number: newNumber,
       };
       setPersons(persons.concat(noteObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
       <form onSubmit={addNote}>
         <div>
-          name: <input value={newName} onChange={handleNoteChange} />
+        name: <input value={newName} onChange={handleNoteChange} /> <br></br>
+        number: <input value={newNumber} onChange={handleNoteChangeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -40,7 +50,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((each) => (
-          <li key={each.name}> {each.name}</li>
+          <li key={each.name}>{each.name} at {each.number}</li>
         ))}
       </ul>
     </div>
